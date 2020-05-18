930176805a9bf004b1e6767f6bb92da15eb48f18
KatsiarynaDzibrova <dzibrovak@famcs-steps.yaconnect.com>
2020-05-18 14:10:52 +0300

    map fixes

--- map_script.cpp
+++ map_script.cpp
@@ -3,13 +3,14 @@
 int main() {
   std::string key;
   while (!std::cin.eof()) {
-    while (getchar() != '\t') {}
+    getline(std::cin, key, '\t');
     while (std::cin.peek() != '\n') {
-      std::string x;
-      std::cin >> x;
-      std::cout << x << "\t1\n";
+      std::string value;
+      std::cin >> value;
+      std::cout << value << "\t1\n";
     }
     std::cin.ignore(2);
   }
   return 0;
 }
+
